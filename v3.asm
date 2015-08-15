159207c0d4775f239fcf1f439846ec4b042148ef
Krzysztof Mularski <krzysztof.mularski@gmail.com>
2015-08-15 20:33:31 +0200

    /v3, version working on argument from command line
    i hope... ;) because this application is not protected in any way
    before any errors... and was used only 1B to count... ;)

--- v3.asm
+++ v3.asm
@@ -1,74 +1,63 @@
+; compile:
+; 	nasm -f elf32 -l v3.lst v3.asm
+;	ld -melf_i386 -o v3 v3.o
+;
+; testing:
+;	./v3 891 echo -e "\nresult=$?\n"
+;
+; Application return result by error code... yes, i know - it is ugly ;)
+
 SECTION .data
-	num: db "728",0,'$'
-	len: equ $-num
 	tab: db 1,0,0,0,0,0,1,0,2,1
 	;		0 1 2 3 4 5 6 7 8 9
-%ifdef GCCMAIN
-	msg1: db "debug: %d\n",0
-%endif
 
 SECTION .text
-%ifdef GCCMAIN
-extern printf
-global main
-main:
-%else
 global _start
 _start:
-%endif
-
-%ifndef GCCMAIN
-	pop ebx		; argc
-	pop ebx		; arv[0] name of app
-	pop ebx		; argc[1], our args
-%endif
+	
+	times 3 pop esi ; get: argc, argv[0], argv[1]
 
 	push ebp
-	mov ebp,esp
-	sub esp,10	; local variable: .result{offset:4}, .ichar{offset:2}
+	mov ebp, esp
+	sub esp, 10
 
-	mov esi,num
-	;mov esi,ebx
-	mov dword [ebp - 4],0		; .result := 0
-	mov dword [ebp - 8],0		; .ichar := 0
+	mov dword [ebp - 4], 0x0 ; used only 1 byte. it is a little problem
+	mov dword [ebp - 8], 0x0 ; used only 1 byte
 
-	zapierdalaj:
-		mov eax,[esi]
+	dofor: ; # 01, for:begin
+		; # 01, for[rules]:begin
+		xor eax, eax
+		mov al, [esi]
 		inc esi
-		cmp eax,0		; looking for end of string
-		je finished
+		cmp al, 0
+		jz endfor
+		; # 01, for[rules]:end
 
-		sub eax,'0'
-		mov dword [ebp - 8], eax 	; .ichar
+		sub al, 48 ; convert from ascii to integer
+		mov byte [ebp - 8],  al ; .ichar, tmp variable
 
+		; ----
 		push esi
-		push ebp
 		push eax
-		; -----------
+
 		mov esi, tab
-		mov ebx, dword [ebp - 8] ; second param is a number, read .ichar
-		mov eax, [esi+ebx*4]	; val
-		add dword [ebp - 4], eax	; .result + val
-		; -----------
+		mov ebx, dword [ebp - 8]
+		xor eax, eax
+		mov al, byte [esi+ebx*1]
+		add byte [ebp - 4], al
+
 		pop eax
-		pop ebp
 		pop esi
+		; ----
 
-		%ifdef GCCMAIN
-			push dword [ebp - 4]
-			push dword msg1
-			call printf
-		%endif
+	jmp dofor ; # 01, for:end
+	endfor:
 
-	jmp zapierdalaj
-
-finished:
-    mov ebx, dword [ebp - 4]		; return .result
+	mov ebx, dword [ebp - 4]
 	mov esp, ebp
 	pop ebp
-;%ifdef GCCMAIN
-	ret
-;%else
-    mov eax,1                ; exit command to kernel
-    int 0x80                 ; interrupt 80 hex, call kernel
-;%endif
+
+	; quit from application
+	mov eax, 1
+	int 0x80
+; eof
